e509a28e776305ec2c5b6de88a0f818e68346f1c
luannsantosprogrammer <168750459+luannsantosprogrammer@users.noreply.github.com>
2024-12-04 16:39:51 -0300

    Update streamlit_selenium.py

--- streamlit_selenium.py
+++ streamlit_selenium.py
@@ -2,6 +2,8 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def search_google():
